5b5b5752f514a1b5a8e5982dd51a80f3f15fdf37
Jonas Nyström <jonasnys@gmail.com>
2022-12-20 15:03:58 +0100

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+
+  const divRef = useRef();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <button onClick={e => {
+        console.log(divRef.current);
+        window.fetch('/svg.txt').then(r => {
+          console.log(r);
+          r.text().then(text => {
+            console.log(text);
+            addSvg(divRef.current, text);
+          });
+        });
+      }}>Test</button>
+      <div ref={divRef} />
+
+    </div >
   );
 }
 
 export default App;
+
+function addSvg(div, svg) {
+
+  console.log('OPTIMIZATION useSvgToBitmapAlt');
+  const t0 = performance.now();
+  const tempdiv = document.createElement('div');
+  tempdiv.innerHTML = svg;
+  const svgElement = tempdiv.firstElementChild;
+  const outerHtml = svgElement.outerHTML;
+  const blob = new Blob([outerHtml], { type: 'image/svg+xml;charset=utf-8' });
+  const blobUrl = URL.createObjectURL(blob);
+  const image = new Image();
+  image.onload = () => {
+    image.style.width = '100%';
+    image.style.height = '100%';
+    div.appendChild(image);
+    const t1 = performance.now();
+    // console.log(div.firstElementChild);
+    // res(true);
+    console.log('useSvgToBitmapAlt page render', t1 - t0 + 'ms');
+  };
+  image.onerror = e => alert(JSON.stringify(e));
+  image.src = blobUrl;
+
+}
